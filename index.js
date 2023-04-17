ffa221290deb668cc9ffd055cdaf06bcf1147da6
sara <saraleepark1994@gmail.com>
2023-04-17 20:18:07 +0900

    nodemon added

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const http = require('http')
 
 const app = http.createServer((request, response) => {
-    response.write('Hello my friends')
+    response.write('Hejsan svejsan')
 
     response.end()
 })
 
-app.listen({ port: 3001 }, () => {
-    console.log('Server startad på localhost:3001')
+app.listen({ port: 3000 }, () => {
+    console.log('Server startad på localhost:3000')
 })
