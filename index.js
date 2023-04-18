74cc1886704bc34c370281c0e43888a13b93892c
sara <saraleepark1994@gmail.com>
2023-04-18 16:32:02 +0900

    send with json data

--- index.js
+++ index.js
@@ -35,3 +35,38 @@ const app = http.createServer((request, response) => {
 app.listen({ port: 3000 }, () => {
     console.log('server started')
 }) */
+
+// Give response as a statu scode 200 in a header
+// Send different message depending on the address that user will visit
+// Send with json data
+// Send with html content to our website from node
+const http = require('http')
+const Port = process.env.Port || 5001
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+
+    //Add routes
+    // better way to handle url
+
+    const url = req.url
+    if (url === '/profile') {
+        res.write('<h1>Welcome to your profile</h1>')
+        res.end()
+    } else if (url === '/shipment') {
+        // send json data , dummy payload (payload = big amount of data like a package)
+
+        const payload = {
+            address: {
+                street: 'Richard street',
+                city: ' Stockholm'
+            }
+        }
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.write(JSON.stringify(payload))
+    } else {
+        res.write('You have reached another page')
+    }
+}).listen(Port, () => {
+    console.log(`Server is fine and its running on port ${Port}`)
+})
